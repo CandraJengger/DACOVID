383228f975d16087fbe77552af98e7901b95b0c8
CandraJengger <cakjengger@gmail.com>
2020-03-31 23:47:32 +0700

    next commit

--- js/api.js
+++ js/api.js
@@ -4,21 +4,37 @@ request.open('GET','https://api.kawalcorona.com/indonesia/provinsi/',true)
 request.onload = function() {
     //parse JSON
     const data = JSON.parse(this.response)
-
     if(request.status >=200 && request.status < 400) {
-        const jatim = data.slice(3,4)
-        const dataJatim = jatim[0].attributes
-        console.log(dataJatim)
 
-        const namaProv = document.getElementById('prov')
-        const positif = document.getElementById('positif')
-        const sembuh = document.getElementById('sembuh')
-        const meninggal = document.getElementById('meninggal')
+        for(let i = 0; i < data.length; i++) {
+            if(data[i].attributes.Provinsi == "Jawa Timur") {
+                console.log(data[i].attributes)
+                const namaProv = document.getElementById('prov')
+                const positif = document.getElementById('positif')
+                const sembuh = document.getElementById('sembuh')
+                const meninggal = document.getElementById('meninggal')
 
-        namaProv.innerText = dataJatim.Provinsi
-        positif.innerText = dataJatim.Kasus_Posi
-        sembuh.innerText = dataJatim.Kasus_Semb
-        meninggal.innerText = dataJatim.Kasus_Meni
+                namaProv.innerText = data[i].attributes.Provinsi
+                positif.innerText = data[i].attributes.Kasus_Posi
+                sembuh.innerText = data[i].attributes.Kasus_Semb
+                meninggal.innerText = data[i].attributes.Kasus_Meni
+                return data[i]
+            }
+        }
+
+        // const jatim = data.slice(4,5)
+        // const dataJatim = jatim[0].attributes
+        // console.log(dataJatim)
+
+        // const namaProv = document.getElementById('prov')
+        // const positif = document.getElementById('positif')
+        // const sembuh = document.getElementById('sembuh')
+        // const meninggal = document.getElementById('meninggal')
+
+        // namaProv.innerText = dataJatim.Provinsi
+        // positif.innerText = dataJatim.Kasus_Posi
+        // sembuh.innerText = dataJatim.Kasus_Semb
+        // meninggal.innerText = dataJatim.Kasus_Meni
 
         
 
